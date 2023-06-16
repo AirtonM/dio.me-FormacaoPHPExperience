47d7978863de4f232e474867be09160c0f6c54c0
Airton Marinho <airtonmarinho15@gmail.com>
2023-06-15 21:23:28 -0300

    Finalizando arquivos com PDO
    Fechando o arquivo index.php para realizar o crud via $_GET.

--- orientacaoAObjetosExcecoesBancoDeDadosComPHP/pdo/src/index.php
+++ orientacaoAObjetosExcecoesBancoDeDadosComPHP/pdo/src/index.php
@@ -4,4 +4,40 @@
 
     $produto = new Produto();
 
-    $produto->list();
+    switch ($_GET['operacao']){
+        case 'list':
+            
+            echo "<h3>Produtos: </h3>";
+
+            foreach ($produto->list() as $key => $value){
+                echo "Id: " . $value["id"] . "\nDescrição: " . $value["descricao"] . "<hr>";
+            }
+            break;
+        
+        case 'insert':
+            $status = $produto->insert('Produto Teste 01');
+            if (!$status){
+                echo "Não foi possível executar a operação";
+                return false;
+            }
+            echo "Registro executado com sucesso";
+            break;
+
+        case 'update':
+            $status = $produto->update('Produto Alterado 01', 1);
+            if (!$status){
+                echo "Não foi possível executar a operação";
+                return false;
+            }
+            echo "Registro atualizado com sucesso";
+            break;
+        
+        case 'delete';
+            $status = $produto->delete(1);
+            if (!$status){
+                echo "Não foi possível executar a operação";
+                return false;
+            }
+            echo "Registro deletado com sucesso";
+            break;
+    }
